a20bc3483efb55366d4de3bfc6df10ff597bdb83
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2022-10-27 12:50:07 +0100

    Update the submission item status on the SDES callback

--- app/controllers/SdesCallbackController.scala
+++ app/controllers/SdesCallbackController.scala
@@ -16,19 +16,38 @@
 
 package controllers
 
-import models.sdes.NotificationCallback
+import models.sdes.{NotificationCallback, NotificationType}
+import models.submission.SubmissionItemStatus
 import play.api.mvc.ControllerComponents
+import repositories.SubmissionItemRepository
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendBaseController
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class SdesCallbackController @Inject() (
-                                     override val controllerComponents: ControllerComponents
-                                   ) extends BackendBaseController {
+                                         override val controllerComponents: ControllerComponents,
+                                         submissionItemRepository: SubmissionItemRepository
+                                       )(implicit ec: ExecutionContext) extends BackendBaseController {
 
   def callback = Action.async(parse.json[NotificationCallback]) { implicit request =>
-    Future.successful(Ok)
+    submissionItemRepository.get(request.body.correlationID).flatMap {
+      _.map { item =>
+        submissionItemRepository.update(
+          item.correlationId,
+          getItemStatus(request.body.notification),
+          request.body.failureReason
+        ).map(_ => Ok)
+      }.getOrElse(Future.successful(NotFound))
+    }
   }
+
+  private def getItemStatus(notificationType: NotificationType): SubmissionItemStatus =
+    notificationType match {
+      case NotificationType.FileReady             => SubmissionItemStatus.Ready
+      case NotificationType.FileReceived          => SubmissionItemStatus.Received
+      case NotificationType.FileProcessed         => SubmissionItemStatus.Processed
+      case NotificationType.FileProcessingFailure => SubmissionItemStatus.Failed
+    }
 }
--- test/controllers/SdesCallbackControllerSpec.scala
+++ test/controllers/SdesCallbackControllerSpec.scala
@@ -16,29 +16,71 @@
 
 package controllers
 
+import models.Done
 import models.sdes.{NotificationCallback, NotificationType}
-import org.scalatest.OptionValues
+import models.submission.{ObjectSummary, SubmissionItem, SubmissionItemStatus}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{BeforeAndAfterEach, OptionValues}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.mockito.MockitoSugar
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsObject, Json}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import play.api.inject.bind
+import repositories.SubmissionItemRepository
 
-class SdesCallbackControllerSpec extends AnyFreeSpec with Matchers with OptionValues {
+import java.time.{Clock, Instant, ZoneOffset}
+import java.time.temporal.ChronoUnit
+import scala.concurrent.Future
+
+class SdesCallbackControllerSpec extends AnyFreeSpec with Matchers with OptionValues with MockitoSugar with BeforeAndAfterEach with ScalaFutures {
+
+  private val mockSubmissionItemRepository = mock[SubmissionItemRepository]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    Mockito.reset(mockSubmissionItemRepository)
+  }
 
   "callback" - {
 
-    val app = GuiceApplicationBuilder().build()
+    val clock = Clock.fixed(Instant.now, ZoneOffset.UTC)
 
-    "must return OK when the request is valid" in {
-
-      val requestBody = NotificationCallback(
-        notification = NotificationType.FileProcessed,
-        filename = "filename",
-        correlationID = "correlationID",
-        failureReason = None
+    val app = GuiceApplicationBuilder()
+      .overrides(
+        bind[SubmissionItemRepository].toInstance(mockSubmissionItemRepository)
       )
+      .build()
+
+    val requestBody = NotificationCallback(
+      notification = NotificationType.FileReady,
+      filename = "filename",
+      correlationID = "correlationID",
+      failureReason = None
+    )
+
+    val item = SubmissionItem(
+      correlationId = "correlationID",
+      status = SubmissionItemStatus.Submitted,
+      objectSummary = ObjectSummary(
+        location = "file",
+        contentLength = 1337L,
+        contentMd5 = "hash",
+        lastModified = clock.instant().minus(2, ChronoUnit.DAYS)
+      ),
+      failureReason = None,
+      lastUpdated = clock.instant()
+    )
+
+    "must update the status of the submission to Ready and return OK" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.successful(Some(item)))
+      when(mockSubmissionItemRepository.update(any(), any(), any())).thenReturn(Future.successful(Done))
 
       val request = FakeRequest(routes.SdesCallbackController.callback)
         .withJsonBody(Json.toJson(requestBody))
@@ -46,12 +88,92 @@ class SdesCallbackControllerSpec extends AnyFreeSpec with Matchers with OptionVa
       val response = route(app, request).value
 
       status(response) mustEqual OK
+      verify(mockSubmissionItemRepository, times(1)).get(requestBody.correlationID)
+      verify(mockSubmissionItemRepository, times(1)).update(requestBody.correlationID, SubmissionItemStatus.Ready, None)
+    }
+
+    "must update the status of the submission to Received and return OK" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.successful(Some(item)))
+      when(mockSubmissionItemRepository.update(any(), any(), any())).thenReturn(Future.successful(Done))
+
+      val request = FakeRequest(routes.SdesCallbackController.callback)
+        .withJsonBody(Json.toJson(requestBody.copy(notification = NotificationType.FileReceived)))
+
+      val response = route(app, request).value
+
+      status(response) mustEqual OK
+      verify(mockSubmissionItemRepository).get(requestBody.correlationID)
+      verify(mockSubmissionItemRepository).update(requestBody.correlationID, SubmissionItemStatus.Received, None)
+    }
+
+    "must update the status of the submission to Processed and return OK" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.successful(Some(item)))
+      when(mockSubmissionItemRepository.update(any(), any(), any())).thenReturn(Future.successful(Done))
+
+      val request = FakeRequest(routes.SdesCallbackController.callback)
+        .withJsonBody(Json.toJson(requestBody.copy(notification = NotificationType.FileProcessed)))
+
+      val response = route(app, request).value
+
+      status(response) mustEqual OK
+      verify(mockSubmissionItemRepository).get(requestBody.correlationID)
+      verify(mockSubmissionItemRepository).update(requestBody.correlationID, SubmissionItemStatus.Processed, None)
+    }
+
+    "must update the status of the submission to Failed and return OK" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.successful(Some(item)))
+      when(mockSubmissionItemRepository.update(any(), any(), any())).thenReturn(Future.successful(Done))
+
+      val request = FakeRequest(routes.SdesCallbackController.callback)
+        .withJsonBody(Json.toJson(requestBody.copy(notification = NotificationType.FileProcessingFailure, failureReason = Some("failure reason"))))
+
+      val response = route(app, request).value
+
+      status(response) mustEqual OK
+      verify(mockSubmissionItemRepository).get(requestBody.correlationID)
+      verify(mockSubmissionItemRepository).update(requestBody.correlationID, SubmissionItemStatus.Failed, Some("failure reason"))
+    }
+
+    "must return NOT_FOUND when there is no matching submission" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.successful(None))
+
+      val request = FakeRequest(routes.SdesCallbackController.callback)
+        .withJsonBody(Json.toJson(requestBody))
+
+      val response = route(app, request).value
+
+      status(response) mustEqual NOT_FOUND
     }
 
     "must return BAD_REQUEST when the request is invalid" in {
       val request = FakeRequest(routes.SdesCallbackController.callback).withJsonBody(JsObject.empty)
       val response = route(app, request).value
       status(response) mustEqual BAD_REQUEST
+    }
+
+    "must fail when the call to get an item fails" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.failed(new RuntimeException()))
+
+      val request = FakeRequest(routes.SdesCallbackController.callback)
+        .withJsonBody(Json.toJson(requestBody))
+
+      route(app, request).value.failed.futureValue
+    }
+
+    "must fail when the call to update an item fails" in {
+
+      when(mockSubmissionItemRepository.get(any())).thenReturn(Future.successful(Some(item)))
+      when(mockSubmissionItemRepository.update(any(), any(), any())).thenReturn(Future.failed(new RuntimeException()))
+
+      val request = FakeRequest(routes.SdesCallbackController.callback)
+        .withJsonBody(Json.toJson(requestBody))
+
+      route(app, request).value.failed.futureValue
     }
   }
 }
