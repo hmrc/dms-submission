2db6e047821c1c10a5981a19ec5447063711d778
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2025-05-16 13:14:38 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.0.0"
-  private val bootstrapVersion = "9.0.0"
+  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.12.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"           % hmrcMongoVersion,
-    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"  % "2.0.0",
+    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"  % "2.2.0",
     "com.github.pathikrit"    %% "better-files"                 % "3.9.2",
     "org.typelevel"           %% "cats-core"                    % "2.12.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "3.0.0",
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.0.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-metrix-play-30"    % hmrcMongoVersion,
     "org.apache.pdfbox"       %  "pdfbox"                       % "2.0.27"
   )
